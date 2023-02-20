918e604071af7edf151c54523a1e2a8e1034fc72
Dan Peterson <danp@danp.net>
2023-02-20 10:18:25 -0400

    update for new site
    The procurement site has been changed over to a new version which uses
    a JSON-based API. No more scraping, hurray!
    
    This did uncover an open Go issue, linked in main.go, since response
    contain duplicate `Transfer-Encoding: chunked` responses. Requires a
    local patch to net/http to actually work.

--- go.mod
+++ go.mod
@@ -3,14 +3,12 @@ module github.com/danp/tender-digest
 go 1.20
 
 require (
-	github.com/PuerkitoBio/goquery v1.8.0
 	github.com/joeshaw/envdecode v0.0.0-20200121155833-099f1fc765bd
 	github.com/sendgrid/sendgrid-go v3.11.1+incompatible
 	modernc.org/sqlite v1.18.1
 )
 
 require (
-	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/google/go-cmp v0.5.8 // indirect
 	github.com/google/uuid v1.3.0 // indirect
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"database/sql"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -16,10 +17,13 @@ import (
 	"strings"
 	"time"
 
-	"github.com/PuerkitoBio/goquery"
 	"github.com/joeshaw/envdecode"
 	_ "modernc.org/sqlite"
 )
+
+// currently requires a locally applied fix for https://github.com/golang/go/issues/44591 to
+// work properly, such as the range over raw in
+// https://github.com/golang/go/issues/44591#issuecomment-825100135.
 
 var config struct {
 	SendgridAPIKey string   `env:"SENDGRID_API_KEY,required"`
@@ -39,7 +43,7 @@ func main() {
 
 	ctx := context.Background()
 
-	cl, err := NewClient("https://procurement.novascotia.ca/ns-tenders.aspx")
+	cl, err := NewClient("https://procurement-portal.novascotia.ca")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -78,24 +82,11 @@ func main() {
 	}
 }
 
-type coll struct {
-	err error
-}
-
-func (c *coll) each(f func(_ int, s *goquery.Selection) error) func(_ int, s *goquery.Selection) {
-	return func(x int, s *goquery.Selection) {
-		if c.err != nil {
-			return
-		}
-		c.err = f(x, s)
-	}
-}
-
 type Client struct {
 	u     *url.URL
 	c     *http.Client
 	ready bool
-	vals  url.Values
+	token string
 }
 
 func NewClient(baseURL string) (*Client, error) {
@@ -111,15 +102,12 @@ func NewClient(baseURL string) (*Client, error) {
 
 	c := &http.Client{Jar: jar}
 
-	return &Client{u, c, false, nil}, nil
+	return &Client{u, c, false, ""}, nil
 }
 
 var baseHeaders = map[string]string{
-	"Accept":                    "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-	"Accept-Language":           "en-US,en;q=0.9",
-	"Cache-Control":             "max-age=0",
-	"Upgrade-Insecure-Requests": "1",
-	"User-Agent":                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36",
+	"Accept":       "application/json",
+	"Content-Type": "application/json",
 }
 
 type Tender struct {
@@ -151,34 +139,42 @@ func (f *Client) List(ctx context.Context, token string) (_ []Tender, nextToken 
 		page = p
 	}
 
-	vals := f.vals
-
-	const pageSize = 100
-
-	vals.Set("ctl00$ctl00$ctl00$ContentPlaceHolderDefault$childContent$u_NSTendersgrid_v2_2$ddDateRange", "0")
-	vals.Set("ctl00$ctl00$ctl00$ContentPlaceHolderDefault$childContent$u_NSTendersgrid_v2_2$tbSearchTenderID", "")
-	vals.Set("ctl00$ctl00$ctl00$ContentPlaceHolderDefault$childContent$u_NSTendersgrid_v2_2$tbDescription", "")
-	vals.Set("ctl00$ctl00$ctl00$ContentPlaceHolderDefault$childContent$u_NSTendersgrid_v2_2$ddCategoryList", "0")
-	vals.Set("ctl00$ctl00$ctl00$ContentPlaceHolderDefault$childContent$u_NSTendersgrid_v2_2$ddDeptAgency", "Halifax Regional Municipality (HRM)")
-	vals.Set("ctl00$ctl00$ctl00$ContentPlaceHolderDefault$childContent$u_NSTendersgrid_v2_2$ddPageSize", fmt.Sprint(pageSize))
-
-	if page > 1 {
-		vals.Set("__EVENTTARGET", "ctl00$ctl00$ctl00$ContentPlaceHolderDefault$childContent$u_NSTendersgrid_v2_2$GridView1")
-		vals.Set("__EVENTARGUMENT", "Page$"+fmt.Sprint(page))
-	} else {
-		vals.Set("ctl00$ctl00$ctl00$ContentPlaceHolderDefault$childContent$u_NSTendersgrid_v2_2$btnFilter", "Filter")
+	type filter struct {
+		Key   string `json:"key"`
+		Value string `json:"value"`
 	}
 
-	req, err := http.NewRequestWithContext(ctx, "POST", f.u.String(), strings.NewReader(vals.Encode()))
+	body := struct {
+		Filters []filter `json:"filters"`
+	}{
+		Filters: []filter{{"procurementEntity", "Halifax Regional Municipality"}},
+	}
+
+	b, err := json.Marshal(body)
+	if err != nil {
+		return nil, "", err
+	}
+
+	q := make(url.Values)
+	q.Set("page", strconv.Itoa(page))
+	q.Set("numberOfRecords", "25")
+	q.Set("sortType", "DATE_CREATED_DESC")
+
+	u, err := f.u.Parse("/procurementui/tenders")
+	if err != nil {
+		return nil, "", err
+	}
+
+	u.RawQuery = q.Encode()
+
+	req, err := http.NewRequestWithContext(ctx, "POST", u.String(), bytes.NewReader(b))
 	if err != nil {
 		return nil, "", err
 	}
 	for k, v := range baseHeaders {
 		req.Header.Set(k, v)
 	}
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	req.Header.Set("Origin", "https://procurement.novascotia.ca")
-	req.Header.Set("Referer", f.u.String())
+	req.Header.Set("Authorization", "Bearer "+f.token)
 
 	resp, err := f.c.Do(req)
 	if err != nil {
@@ -190,94 +186,61 @@ func (f *Client) List(ctx context.Context, token string) (_ []Tender, nextToken 
 		return nil, "", fmt.Errorf("got status %d", resp.StatusCode)
 	}
 
-	b, err := ioutil.ReadAll(resp.Body)
+	b, err = ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return nil, "", err
 	}
 
-	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(b))
-	if err != nil {
+	type tenderData struct {
+		TenderID          string
+		Title             string
+		ProcurementEntity string
+		PostDate          string
+		ClosingDate       string
+	}
+	var respBody struct {
+		TenderDataList []tenderData
+	}
+
+	if err := json.Unmarshal(b, &respBody); err != nil {
 		return nil, "", err
 	}
 
-	vals = make(url.Values)
-	doc.Find("form#aspnetForm input[type=hidden]").Each(func(_ int, s *goquery.Selection) {
-		n, _ := s.Attr("name")
-		v, _ := s.Attr("value")
-		vals.Set(n, v)
-	})
-	f.vals = vals
+	var ts []Tender
+	for _, td := range respBody.TenderDataList {
+		pd, err := time.Parse("2006-01-02", td.PostDate)
+		if err != nil {
+			return nil, "", err
+		}
+		if len(td.ClosingDate) < len(time.DateOnly) {
+			return nil, "", fmt.Errorf("bad closing date %q", td.ClosingDate)
+		}
+		cd, err := time.Parse(time.DateOnly, td.ClosingDate[:len(time.DateOnly)])
+		if err != nil {
+			return nil, "", err
+		}
 
-	var (
-		c  = &coll{}
-		ts []Tender
-	)
-
-	doc.Find("table#ctl00_ctl00_ctl00_ContentPlaceHolderDefault_childContent_u_NSTendersgrid_v2_2_GridView1 > tbody > tr").
-		Not(".gridfooter").
-		Each(c.each(func(_ int, s *goquery.Selection) error {
-			if s.Find("th").Length() > 0 {
-				return nil
-			}
-			if s.HasClass("ProcPagination") {
-				return nil
-			}
-
-			var t Tender
-			t.ID = s.Find("td:nth-child(2) a").Text()
-
-			href, ok := s.Find("td:nth-child(2) a").Attr("href")
-			if !ok {
-				return fmt.Errorf("%s missing href", t.ID)
-			}
-			hurl, err := f.u.Parse(href)
-			if err != nil {
-				return err
-			}
-
-			t.URL = hurl.String()
-			t.Agency = strings.TrimSpace(s.Find("td:nth-child(2) span").Text())
-			t.Description = strings.TrimSpace(s.Find("td:nth-child(3)").Text())
-
-			spanStrings := s.Find("td:nth-child(4) span").Map(func(_ int, ss *goquery.Selection) string {
-				return ss.Text()
-			})
-			if len(spanStrings) != 2 {
-				return fmt.Errorf("expected 2 date spans for id %q, found %d", t.ID, len(spanStrings))
-			}
-
-			const dateFormat = "02 Jan 2006"
-
-			id, err := time.Parse(dateFormat, spanStrings[1])
-			if err != nil {
-				return err
-			}
-			t.IssuedDate = id
-
-			cd, err := time.Parse(dateFormat, spanStrings[0])
-			if err != nil {
-				return err
-			}
-			t.CloseDate = cd
-
-			ts = append(ts, t)
-			return nil
-		}))
-	if c.err != nil {
-		return nil, "", c.err
+		ts = append(ts, Tender{
+			ID:          td.TenderID,
+			URL:         f.u.String() + "/tenders/" + td.TenderID,
+			Description: td.Title,
+			Agency:      td.ProcurementEntity,
+			IssuedDate:  pd,
+			CloseDate:   cd,
+		})
 	}
 
 	token = ""
-	if len(ts) >= pageSize {
+	if len(ts) > 0 {
 		page++
 		token = fmt.Sprint(page)
 	}
 
-	return ts, token, c.err
+	return ts, token, nil
 }
 
 func (f *Client) init(ctx context.Context) error {
-	req, err := http.NewRequestWithContext(ctx, "GET", f.u.String(), nil)
+	req, err := http.NewRequestWithContext(ctx, "POST", f.u.String()+"/procurementui/authenticate", strings.NewReader(`{"rpid":"GUEST"}`))
 	if err != nil {
 		return err
 	}
@@ -300,18 +263,18 @@ func (f *Client) init(ctx context.Context) error {
 		return err
 	}
 
-	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(b))
-	if err != nil {
-		return err
+	var body struct {
+		JWTToken string
+	}
+	if err := json.Unmarshal(b, &body); err != nil {
+		return fmt.Errorf("unmarshaling body: %w", err)
 	}
 
-	vals := make(url.Values)
-	doc.Find("form#aspnetForm input[type=hidden]").Each(func(_ int, s *goquery.Selection) {
-		n, _ := s.Attr("name")
-		v, _ := s.Attr("value")
-		vals.Set(n, v)
-	})
-	f.vals = vals
+	if body.JWTToken == "" {
+		return fmt.Errorf("body does not contain jwttoken")
+	}
+
+	f.token = body.JWTToken
 
 	return nil
 }
