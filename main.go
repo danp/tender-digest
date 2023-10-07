113954160be46bfc424be9e8b50b3739dd404986
Dan Peterson <danp@danp.net>
2023-10-07 15:59:46 -0300

    we are chrome now

--- main.go
+++ main.go
@@ -106,6 +106,7 @@ func NewClient(baseURL string) (*Client, error) {
 var baseHeaders = map[string]string{
 	"Accept":       "application/json",
 	"Content-Type": "application/json",
+	"User-Agent":   "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36",
 }
 
 type Tender struct {
